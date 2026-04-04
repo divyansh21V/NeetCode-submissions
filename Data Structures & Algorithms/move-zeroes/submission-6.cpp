44614fa6f984e54f5f7bb5cd03fc32532e6c582a
Divyansh Kalra <divyanshk8021@gmail.com>
2026-04-04 13:15:34 +0530

    Add: move-zeroes - submission-6

---
+++ Data Structures & Algorithms/move-zeroes/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       if (nums.size() == 1){ return ; }
+        for (int i = 0 ; i < 10 ; i++){
+            if(nums[i] == 0 ){ 
+                for (int j = nums.size()-1 ; j > i; j--){
+                    if(nums[j] != 0){ 
+                        std::swap(nums[i],nums[j]);
+                    
+                    }
+
+                }
+            }
+        }
+        return ; 
+    }
+};
